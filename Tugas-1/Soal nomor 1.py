373de89c3450a51ed88ace439d5cf08fdef2d5a5
Elisabeth <elisabethangelina14@gmail.com>
2021-04-23 20:41:30 +0700

    hilang tugas 1

--- Tugas-1/Soal nomor 1.py
+++
@@ -1,7 +0,0 @@
-nama = input("Masukan nama kamu: ")
-
-umur = int(input("Berapa umur kamu : "))
-
-tinggi = float(input("berapa tinggi kamu:"))
-
-print("Nama saya {}, umur saya {} tahun, dan tinggi saya adalah {} cm".format(nama, umur, tinggi))
