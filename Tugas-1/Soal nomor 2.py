d486f113aa817ba0bf9f5f71c60d53e76c1807ab
Elisabeth <elisabethangelina14@gmail.com>
2021-04-23 20:35:19 +0700

    upload tugas 1-3

---
+++ Tugas-1/Soal nomor 2.py
@@ -0,0 +1,9 @@
+phi = 22/7
+r = int(input("masukan jari-jari lingkaran: "))
+L = phi * (r*r)
+print ("Luas lingkaran dengan jari-jari {} cm adalah {} cm\u00b2.".format(r,L))
+
+phi = 22/7
+r = int(input("masukan jari-jari lingkaran: "))
+L = phi * (r*r)
+print ("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm\u00b2." .format(r,L))
