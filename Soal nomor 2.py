b23dc910201e6bcd1d997392d49120bf6502ce93
Elisabeth <elisabethangelina14@gmail.com>
2021-04-23 20:49:16 +0700

    hilang file 2

--- Soal nomor 2.py
+++
@@ -1,9 +0,0 @@
-phi = 22/7
-r = int(input("masukan jari-jari lingkaran: "))
-L = phi * (r*r)
-print ("Luas lingkaran dengan jari-jari {} cm adalah {} cm\u00b2.".format(r,L))
-
-phi = 22/7
-r = int(input("masukan jari-jari lingkaran: "))
-L = phi * (r*r)
-print ("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm\u00b2." .format(r,L))
