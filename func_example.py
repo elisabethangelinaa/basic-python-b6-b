847324b2fd64248602def8d8fb4aa36ddd01e760
Elisabeth <elisabethangelina14@gmail.com>
2021-04-30 18:07:23 +0700

    Tugas 2

---
+++ func_example.py
@@ -0,0 +1,10 @@
+phi = 22/7
+r = int(input("Masukkan jari-jari:"))
+def luas_lingkaran (phi,r):
+    return phi*r*r
+def keliling_lingkaran (phi,r):
+    return 2*phi*r
+
+print("Luas lingkaran adalah: ",luas_lingkaran(22/7,r))
+print("Keliling lingkaran adalah: ", keliling_lingkaran(22/7,r))
+
